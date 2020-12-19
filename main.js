285a6daed12e2366b9f35fead8f36203bc7e551b
Nuh <1345959121@qq.com>
2020-12-19 23:55:03 +0800

    remote模块的使用

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-const {app,BrowserWindow} = require("electron")
+ const electron= require("electron")
+ const {app,BrowserWindow} = electron
+ 
 // 引入两个模块，分别对应窗口和应用
 
 let mainWin = null
 
 // 应用监听准备好事件
 app.on("ready", () => {
+    console.log('主进程中的模块 \n',electron)
     // 实例化窗口
     mainWin = new BrowserWindow({
         width:400,
         height:600,
-        webPreferences:{
-            nodeIntegration:true
-        }
+        webPreferences:{ 
+            // 渲染进程中允许使用node
+            nodeIntegration:true,
+            // 渲染进程中允许使用remote模块
+            enableRemoteModule:true,
+        },
+        
     })
     // 实例化后加载对应的页面
     mainWin.loadFile("index.html")
