f327dcd1c8cb060ae482fd09308967a370673569
Nuh <1345959121@qq.com>
2020-12-20 10:33:09 +0800

    自定义菜单栏

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
  const electron= require("electron")
- const {app,BrowserWindow} = electron
- 
+ const {app,BrowserWindow,Menu} = electron
+
 // 引入两个模块，分别对应窗口和应用
 
 let mainWin = null
@@ -9,6 +9,31 @@ let mainWin = null
 app.on("ready", () => {
     console.log('主进程中的模块 \n',electron)
     // 实例化窗口
+    let menuTem = [
+        {
+            label: "主菜单一",
+            submenu: [
+                {
+                    label: '子菜单1-1',
+                    click:  () => {
+                        win = new BrowserWindow({width:200,height:200})
+                        win.loadFile("test.html")
+                        win.on("close",() => {win = null})
+                    }
+                },
+                {label: '子菜单1-2'}
+            ]
+        },bro
+        {
+            label: "主菜单二",
+            submenu: [
+                {label: '子菜单2-1'},
+                {label: '子菜单2-2'}
+            ]
+        },
+    ]
+    let m = Menu.buildFromTemplate(menuTem)
+    Menu.setApplicationMenu(m)
     mainWin = new BrowserWindow({
         width:400,
         height:600,
