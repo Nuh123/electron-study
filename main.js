4cf51e6d65401c69cd7246cb16f087096bdf6524
Mayong <1345959121@qq.com>
2020-12-27 17:13:00 +0800

    Browview 对象

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 const console = require("console")
-const {app,BrowserWindow} = require("electron")
+const { app, BrowserWindow, BrowserView } = require("electron")
 // 使用node处理路径问题
 const path = require("path")
 
-function creatWindow () {
+function creatWindow() {
     mainWin = new BrowserWindow({
-        backgroundColor:"#ff0",
+        backgroundColor: "#ff0",
         // frame:false,
         // transparent:true,
         width: 500,
         height: 500,
-        webPreferences:{
-            nodeIntegration:true,
-            webviewTag:true,
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true,
         }
     })
-    mainWin.loadFile(path.join("index.html"))
+    // mainWin.loadFile(path.join("index.html"))
+    mainWin.loadURL('https://baidu.com')
     // 与视频有出入，和顺序无关。
     mainWin.webContents.openDevTools()
+
+    // BrowserView 对象
+    const view = new BrowserView()
+    mainWin.setBrowserView(view)
+    view.setBounds({ x: 0, y: 0, width: 300, height: 300 })
+    view.webContents.loadFile(path.join("index.html"))
 }
 let mainWin = null
 
@@ -25,29 +32,29 @@ let mainWin = null
 // 另 promise的then方法会自己执行，不需要加括号。
 app.whenReady().then(creatWindow)
 // 关闭应用时的操作
-app.on("window-all-closed",function() {
-    mainWin= null
+app.on("window-all-closed", function () {
+    mainWin = null
     app.quit()
 })
 
-app.on("reday",function() {
+app.on("reday", function () {
     console.log('reday  事件')
 })
-app.on("dom-ready",function() {
+app.on("dom-ready", function () {
     console.log('dom-ready  事件')
 })
-app.on("did-finish-load",function() {
+app.on("did-finish-load", function () {
     console.log('did-finish-load 事件')
 })
-app.on("window-all-closed",function() {
+app.on("window-all-closed", function () {
     console.log('window-all-closed  事件')
 })
-app.on("before-quit",function() {
+app.on("before-quit", function () {
     console.log('before-quit  事件')
 })
-app.on("quit",function() {
+app.on("quit", function () {
     console.log('quit  事件')
 })
-app.on("close",function() {
+app.on("close", function () {
     console.log('close  事件')
 })
